c48ba956eb947997cd0be26c6b5a3c0c3100f1ee
Keagan Pang <keagan_pangzr@hotmail.com>
2020-11-09 19:30:40 +0800

    added feature engineering: name_prefix, family_size, is_alone, is_upper_2_decks

--- Titanic/titanic_submission3.py
+++ Titanic/titanic_submission3.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import metrics
 from sklearn.model_selection import train_test_split, cross_val_score, KFold, GridSearchCV
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -16,41 +16,37 @@ pd.set_option("display.max_columns", 20)
 '''
 Notes
 
-key decisions
-1. how to deal with missin age and embarked data -> imputation vs drop values
-2. hold-out vs cross-validation
-4. scaling features or not
-3. model selection -> single model or ensemble 
-
 Features checklist
-- Sex -> one-hot -
+- Sex -> one-hot
 - Pclass -> label encoded, leave as is
 - Age -> normalize
-- SibSp & Parch -> label encoded, leave as is
+- Family size
+- is alone
 - Fare -> normalize
 - Embarked -> one-hot (test leaving zero-hot)
-
-
-How to improve:
-1. use cross-validation instead of hold-out, train model on all data - DONE
-2. impute age and fare in training set - DONE with mean
-3. test different individual models (add XGboost)
-4. use ensemble of methods (add voting)
-5. add hyper-parameter tuning - DONE with gridsearch
-
 
 '''
 
 df_train = pd.read_csv("data/train.csv")
 df_test = pd.read_csv("data/test.csv")
 
-#Data pre-processing: Drop rows where age data is missing
-df_train['Age'] = df_train['Age'].fillna(df_train['Age'].mean())
-df_train['Fare'] = df_train['Fare'].fillna(df_train['Fare'].mean())
+def preprocess(df_train):
+    #Impute data
+    df_train['Age'] = df_train['Age'].fillna(df_train['Age'].mean())
+    df_train['Embarked'] = df_train['Embarked'].fillna(df_train['Embarked'].mode()[0])
+    df_train['Fare'] = df_train['Fare'].fillna(df_train['Fare'].mean())
+
+    return df_train
+
+df_train = preprocess(df_train)
+
+#df_train['Deck'] = df_train['Cabin'].astype(str).str[0]
+#print(df_train.groupby('Deck')['Survived'].mean())
+#print(df_train['Survived'].mean())
+#print(df_train[df_train['Deck']=='B'])
 
 
-#df_train = df_train[df_train['Age'].isnull() == False]
-df_train = df_train[df_train['Embarked'].isnull() == False]
+
 
 def feature_engineering(df_train):
     #Sex: One-hot
@@ -63,14 +59,32 @@ def feature_engineering(df_train):
     #Embarked: One-hot
     df_train[['Embarked_C', 'Embarked_Q', 'Embarked_S']] = pd.get_dummies(df_train['Embarked'])
 
+    #Family size and is_alone
+    df_train['Family_Size'] = df_train['SibSp'] + df_train['Parch'] + 1
+    df_train['Is_Alone'] = np.where(df_train['Family_Size'] == 1, 1, 0)
+
+    #Name Prefixes
+    df_train['Name_Prefix'] = [name.split(', ')[1].split('.')[0] for name in df_train['Name']]
+    df_train['Name_Prefix'] = [prefix if prefix in ['Mr', 'Mrs', 'Miss', 'Master'] else "Others" for prefix in df_train['Name_Prefix']]
+    df_train['Name_Prefix'] = LabelEncoder().fit_transform(df_train['Name_Prefix'])
+
+    #Deck
+    decks = df_train['Cabin'].astype(str).str[0]
+    df_train['Is_Upper_2_Decks'] = [1 if deck in ['A', 'B'] else 0 for deck in decks] 
+
     return df_train
 
 df_train = feature_engineering(df_train)
 
+print(df_train.head(10))
+
+
+
 #Split data with Hold-out method, do not include Embarked_S to avoid dummy variable trap
-features =['Sex', 'Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked_C', 'Embarked_Q']
+features =['Sex', 'Pclass', 'Age', 'Family_Size', 'Is_Alone', 'Fare', 'Embarked_C', 'Embarked_Q', 'Name_Prefix', 'Is_Upper_2_Decks']
 X = df_train[features]
 y = df_train['Survived']
+
 
 #cross-validator
 cv = KFold(n_splits=5, shuffle=True, random_state=1)
@@ -80,7 +94,7 @@ param_grid_knn = {'n_neighbors':range(2,21,2), 'metric':['euclidean','manhattan'
 grid_search_knn = GridSearchCV(KNeighborsClassifier(), param_grid=param_grid_knn, scoring='accuracy', cv=cv, n_jobs=-1)
 
 #Random Forest
-param_grid_rf = {'n_estimators':range(50, 250, 50), 'max_depth': range(5,20,5), 'max_features': [5,6,7,8], 'bootstrap':[True]}
+param_grid_rf = {'n_estimators':range(50, 250, 50), 'max_depth': range(5,20,5), 'max_features': range(3, len(features)), 'bootstrap':[True]}
 grid_search_rf = GridSearchCV(RandomForestClassifier(), param_grid=param_grid_rf, scoring='accuracy', cv=cv, n_jobs=-1)
 
 #Logistic regression
@@ -107,15 +121,16 @@ majority_vote_clf = VotingClassifier([(name, model.best_estimator_) for name, mo
 print('Majority Vote: ', cross_val_score(majority_vote_clf, X, y, cv=cv, n_jobs=-1).mean())
 majority_vote_clf.fit(X, y)
 
-#Feature engineering for submission test data set
+
+
+
+
+#Submission
+df_test = preprocess(df_test)
 df_test = feature_engineering(df_test)
 
-#Impute missing age and fare in df_test with mean of df_train
-df_test['Age'] = df_test['Age'].fillna(df_train['Age'].mean())
-df_test['Fare'] = df_test['Fare'].fillna(df_train['Fare'].mean())
-
-X = df_test[features]
-y_pred = majority_vote_clf.predict(X)
+X_test = df_test[features]
+y_pred = majority_vote_clf.predict(X_test)
 
 df_submission = pd.DataFrame()
 df_submission['PassengerId'] = df_test['PassengerId']
